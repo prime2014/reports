e040e2a49e560a56c255966d99231ea9f14ac2e0
prime2014 <omondiprime@gmail.com>
2023-12-31 14:58:46 +0300

    created the inventory and purchases report

--- src/report/report.controller.ts
+++ src/report/report.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Res } from '@nestjs/common';
+import { Controller, Get, Res, Sse, StreamableFile } from '@nestjs/common';
 import { Response } from 'express';
 import { ReportService } from './report.service';
+import { createReadStream } from 'fs';
 
 @Controller('report')
 export class ReportController {
@@ -8,16 +9,51 @@ export class ReportController {
     constructor(private readonly pdfService: ReportService){}
 
     @Get()
-    async getPdf(@Res() res: Response): Promise<void> {
-        res.setHeader("Content-Type", "application/pdf");
-        res.setHeader("Content-Disposition", "inline;filename=example.pdf");
+    async getPdf(@Res() res: Response) {
+        res.set({
+            'Content-Type': 'application/pdf',
+            'Content-Disposition': 'inline; filename="example.pdf"',
+        });
 
         try {
             const pdfStream = await this.pdfService.generatePDF()
+            
             pdfStream.pipe(res)
         } catch(error){
             throw error;
         }
         
     }
+
+
+    @Get("/api/v1/inventory")
+    async getInventory(@Res() res: Response) {
+        res.set({
+            'Content-Type': 'application/pdf',
+            'Content-Disposition': 'inline; filename="inventory.pdf"',
+        });
+        try {
+            const pdfStream = await this.pdfService.getInventory()
+            pdfStream.pipe(res);
+        } catch(error) {
+            throw error;
+        }
+    }
+
+    @Get("/api/v1/purchases")
+    async getPurchasesReport(@Res() res: Response) {
+        res.set({
+            'Content-Type': 'application/pdf',
+            'Content-Disposition': 'inline; filename="inventory.pdf"',
+        })
+
+        try {
+            const pdfStream = await this.pdfService.getPurchases()
+            pdfStream.pipe(res);
+        } catch(error) {
+            throw error;
+        }
+    }
+
+    
 }
---
+++ src/report/tableContent.ts
@@ -0,0 +1,36 @@
+const inventoryHeader = ['No.', 'Item', 'Qty'];
+
+const inventoryData = [
+    ["1.", 'Coca Cola', '15'],
+    ["2.", "Butter-Toast Bread you cannot want to miss. This is the best thing that has ever happened to me. so please don't ruin it for me", "300"],
+    ["3.", "Butter-Toast Bread", "300"],
+    ["4.", "Butter-Toast Bread", "300"],
+    ["5.", "Butter-Toast Bread", "300"],
+    ["6.", "Butter-Toast Bread", "300"],
+    ["7.", "Butter-Toast Bread", "300"],
+    ["8.", "Butter-Toast Bread", "300"],
+    ["9.", "Butter-Toast Bread", "300"],
+    ["10.", "Butter-Toast Bread", "300"],
+    ["11.", "Butter-Toast Bread", "300"],
+    ["12.", "Butter-Toast Bread", "300"],
+    ["13.", "Butter-Toast Bread", "300"],
+
+    // Add more rows as needed
+];
+
+const outOfStockHeader = ['No.', 'Item'];
+    
+// Define table rows
+const outOfStockData = [
+    ["1.", 'Coca Cola'],
+    ["2.", "Butter-Toast Bread"],
+            // Add more rows as needed
+];
+
+
+export const TableContent = {
+    inventoryHeader,
+    inventoryData,
+    outOfStockHeader,
+    outOfStockData
+}
